eccf4fab066879d36ed1c53f2ed1f9ec21f25586
Kristian <49764796+kristianka@users.noreply.github.com>
2023-01-27 02:03:29 +0200

    Finish basic CRUD

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const { rmSync } = require('fs');
 
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://127.0.0.1:27017/yelpcamp', {
@@ -21,19 +23,18 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
 app.get('/campgrounds', async (req, res) => {
-    // fetch campgrounds from db
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
 });
 
 app.get('/campgrounds/new', async (req, res) => {
-    // fetch campgrounds from db
     const campgroundInfo = await Campground.findById(req.params.id);
     res.render('campgrounds/new');
 });
@@ -45,10 +46,27 @@ app.post('/campgrounds', async (req, res) => {
 });
 
 app.get('/campgrounds/:id', async (req, res) => {
-    // fetch campgrounds from db
     const campgroundInfo = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campgroundInfo });
 });
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campgroundInfo = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campgroundInfo });
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campgroundInfo = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campgroundInfo._id}`);
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+});
+
 
 app.listen(3000, (req, res) => {
     console.log('Serving on port 3000!');
